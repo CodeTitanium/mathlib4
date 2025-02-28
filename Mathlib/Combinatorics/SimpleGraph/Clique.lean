765dc0f430d129c8eb1f2e564693eb28c975c1e2
jt496 <john.talbot@gmail.com>
2025-02-28 13:03:47 +0000

    removed extra results

--- Mathlib/Combinatorics/SimpleGraph/Clique.lean
+++ Mathlib/Combinatorics/SimpleGraph/Clique.lean
@@ -466,23 +466,6 @@ protected lemma eq_top_iff [Fintype α] : G = ⊤ ↔ Fintype.card α < n := by
 protected lemma ne_top_iff [Fintype α] : G ≠ ⊤ ↔ n ≤ Fintype.card α := by
   rw [ne_eq, h.eq_top_iff, Nat.not_lt]
 
-variable {x y : α}
-lemma exists_isNClique_sup_edge (hne : x ≠ y) (hn : ¬ G.Adj x y) :
-   ∃ t, (G ⊔ edge x y).IsNClique n t ∧ x ∈ t ∧ y ∈ t :=
-  let ⟨t, hc⟩ := not_forall_not.1 <| h.not_cliqueFree_of_gt <| G.lt_sup_edge hne hn
-  ⟨t, hc, ⟨h.1.mem_of_sup_edge_isNClique hc, h.1.mem_of_sup_edge_isNClique (edge_comm _ _ ▸ hc)⟩⟩
-
-lemma exists_of_not_adj [DecidableEq α] (hne : x ≠ y) (hn : ¬ G.Adj x y) :
-    ∃ s, x ∉ s ∧ y ∉ s ∧ G.IsNClique (n - 1) (insert x s) ∧ G.IsNClique (n - 1) (insert y s) := by
-  obtain ⟨t, hc, hx, hy⟩ := h.exists_isNClique_sup_edge hne hn
-  use (t.erase x).erase y, erase_right_comm (a := x) ▸ (not_mem_erase _ _), not_mem_erase _ _
-  rw [insert_erase (mem_erase_of_ne_of_mem hne.symm hy), erase_right_comm,
-      insert_erase (mem_erase_of_ne_of_mem hne hx)]
-  cases n with
-  | zero => exact False.elim <| not_cliqueFree_zero h.1
-  | succ n =>
-    exact ⟨(edge_comm .. ▸ hc).erase_of_sup_edge_of_mem hy, hc.erase_of_sup_edge_of_mem hx⟩
-
 lemma not_cliqueFree_of_le_card [Fintype α] (hle : n - 1 ≤ Fintype.card α) :
     ¬ G.CliqueFree (n - 1) := by
   classical
