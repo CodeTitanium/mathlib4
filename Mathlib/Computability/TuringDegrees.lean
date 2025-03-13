ebd766ec41f3fc57aa3a969a8343e5c24cbf11e7
Tanner Duve <122851174+tannerduve@users.noreply.github.com>
2025-03-12 23:52:14 -0400

    Date update
    Co-authored-by: Tristan F.-R. <tristanf@reed.edu>

--- Mathlib/Computability/TuringDegrees.lean
+++ Mathlib/Computability/TuringDegrees.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2024 Tanner Duve. All rights reserved.
+Copyright (c) 2025 Tanner Duve. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Tanner Duve, Elan Roth
 -/
