a7e5df9a9fb33434c1d371adbbb5f4445711ddd7
Yaël Dillies <yael.dillies@gmail.com>
2025-04-05 20:08:32 +0000

    fix

--- Mathlib.lean
+++ Mathlib.lean
@@ -753,7 +753,6 @@ import Mathlib.Algebra.Order.Field.Pointwise
 import Mathlib.Algebra.Order.Field.Power
 import Mathlib.Algebra.Order.Field.Rat
 import Mathlib.Algebra.Order.Field.Subfield
-import Mathlib.Algebra.Order.Floor
 import Mathlib.Algebra.Order.Floor.Defs
 import Mathlib.Algebra.Order.Floor.Div
 import Mathlib.Algebra.Order.Floor.Extended
