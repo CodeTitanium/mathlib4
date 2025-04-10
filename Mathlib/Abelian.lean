4b43095fd7b6dd303d84553577aebf20b9a9db5e
Xavier Roblot <roblot@math.univ-lyon1.fr>
2025-04-10 12:06:35 +0200

    wip

--- Mathlib/Abelian.lean
+++ Mathlib/Abelian.lean
@@ -1,5 +1,6 @@
 import Mathlib.FieldTheory.Galois.Basic
 
-variable (E F : Type*) [Field E] [Field F] [IsGalois F E] [IsMulCommutative (E ≃ₐ[F] E)]
+variable (E F : Type*) [Field E] [Field F] [Algebra F E] [IsGalois F E]
+  [IsMulCommutative (E ≃ₐ[F] E)]
 
-#synth CommGroup (E ≃ₐ[F] E)
+#synth CommGroup (E ≃ₐ[F] E) -- CommGroup.ofIsMulCommutative
