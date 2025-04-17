53bcaff63329fd80fb0e25c05afb5f58a17148ab
Xavier Roblot <roblot@math.univ-lyon1.fr>
2025-04-17 13:38:04 +0200

    delete files

--- Mathlib/Abelian.lean
+++
@@ -1,6 +0,0 @@
-import Mathlib.FieldTheory.Galois.Basic
-
-variable (E F : Type*) [Field E] [Field F] [Algebra F E] [IsGalois F E]
-  [IsMulCommutative (E ≃ₐ[F] E)]
-
-#synth CommGroup (E ≃ₐ[F] E) -- CommGroup.ofIsMulCommutative
--- Mathlib/mwe.lean
+++
@@ -1,4 +0,0 @@
-import Mathlib.FieldTheory.Galois.Basic
-
-variable (F E : Type*) [Field F] [Field E] [Algebra F E] [IsGalois F E]
-  [Subgroup.IsCommutative (⊤ : Subgroup (E ≃ₐ[F] E))]
